8c9d8bd21688f1fb5301594d8d24879aeaf1edfc
000brian <brianmaciejwilk@gmail.com>
2024-09-07 18:00:14 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	let feedback = [
+		{
+			id: 1,
+			rating: 10,
+			text: 'placeholder text',
+		},
+		{
+			id: 2,
+			rating: 1,
+			text: 'placeholder text',
+		},
+		{
+			id: 3,
+			rating: 7,
+			text: 'placeholder text',
+		},
+	];
+	
+</script>
+
+<main>
+	<h1>Hello</h1>
+
+	
+</main>
+
+<style>
+
+</style>
